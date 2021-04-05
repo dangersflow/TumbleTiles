aeb428df9b54befa6acef2101f7c410fdb6b8acc
dangersflow <pacog879@hotmail.com>
2021-04-05 10:27:19 -0500

    added menu items & started with grid stuff

--- Tumble Tiles/Assets/TabSystem.cs
+++ Tumble Tiles/Assets/TabSystem.cs
@@ -13,20 +13,24 @@ public class TabSystem : MonoBehaviour
     public GameObject sizeOfTabGroup;
     private int numOfTabs;
     private GameObject firstTab;
-    private GameObject InputCarat;
+    private Transform InputCarat;
+    private bool foundCarat = false;
     // Start is called before the first frame update
     void Start()
     {
         numOfTabs = TabContainer.transform.childCount - 1;
         firstTab = TabContainer.GetChild(0).gameObject;
         firstTab.transform.Find("Tab").transform.Find("DeleteButton").GetComponent<Toggle>().onValueChanged.AddListener((self) => DeleteTab(firstTab));
-        InputCarat = firstTab.transform.Find("Tab").transform.Find("Label Input Carat").transform.gameObject;
-        InputCarat.transform.SetSiblingIndex(1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!foundCarat){
+            InputCarat = firstTab.transform.Find("Tab").transform.Find("Label Input Caret").transform;
+            InputCarat.transform.SetSiblingIndex(1);
+            foundCarat = true;
+        }
         
     }
 
@@ -36,6 +40,8 @@ public class TabSystem : MonoBehaviour
         newTab.transform.SetParent(TabContainer, false);
         newTab.transform.SetSiblingIndex(numOfTabs);
         newTab.transform.Find("Tab").transform.Find("DeleteButton").GetComponent<Toggle>().onValueChanged.AddListener((self) => DeleteTab(newTab));
+        firstTab = newTab;
+        foundCarat= false;
         numOfTabs++;
         if(numOfTabs == 4){
             AddTabButton.SetActive(false);
